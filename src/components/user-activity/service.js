5e7a38d9a9926a3c1fea6622f8d946e20f370b32
Your Name <you@example.com>
2021-08-13 11:43:10 +0300

    implement user activity component service level

--- src/components/user-activity/service.js
+++ src/components/user-activity/service.js
@@ -4,48 +4,132 @@
  */
 class UserActivityService {
 
-    /**
-     * @public
-     * @static
-     * @param {UserActivityDataAccess} userActivityDataAccess
-     */
-    static instantiate(userActivityDataAccess) {
-        return new UserActivityService(userActivityDataAccess);
+  /**
+   * @public
+   * @static
+   * @param {UserActivityDataAccess} dataAccess
+   */
+  static instantiate(dataAccess) {
+    return new UserActivityService(dataAccess);
+  }
+
+  #dataAccess;
+
+  /**
+   * @public
+   * @constructor
+   * @param {UserActivityDataAccess} dataAccess
+   */
+  constructor(dataAccess) {
+    this.#dataAccess = dataAccess;
+  }
+
+  /**
+   * @description if user has been successfully tracked will return true in another cases returns false
+   * @param {string} id 
+   * @param {string} ip 
+   * @returns {boolean}
+   */
+  async track(id, ip) {
+    if (id && ip) {
+      return await this.#dataAccess.addTrackPair(id, ip);
+    } else if (id) {
+      return await this.#dataAccess.addTrackId(id);
+    } else if (ip) {
+      return await this.#dataAccess.addTrackIp(ip);
+    }
+    return false;
+  }
+
+
+  /**
+   * @typedef {Object.<string, number>} UserActivityStats
+   */
+
+  /**
+   * @description return user activity stats,composed by days, between startDate and endDate range.
+   * If no startDate date specified or incorrect returns stats for endDate.
+   * If no endDate specified or incorrect returns stats for startDate.
+   * If both are not specified or incorrect returns stats for today.
+   * If date order is incorrect returns stats for today.
+   * @param {string} startDate date(without time) string in ISO format
+   * @param {string} endDate date(without time) string in ISO format
+   * @returns {UserActivityStats}
+   */
+  async getStat(startDate, endDate) {
+    const stat = {};
+    const dateRange = this.#composeDateRange(startDate, endDate);
+    for (const date of dateRange) {
+      stat[date] = await this.#dataAccess.getStat(date);
+    }
+    return stat;
+  }
+
+  /**
+   * @description return range of date between startDate and endDate inclusive
+   * If no startDate date specified or incorrect returns endDate.
+   * If no endDate specified or incorrect returns startDate.
+   * If both are not specified or incorrect returns today's date.
+   * If date order is incorrect returns today's date.
+   * @param {string} startDate date(without time) string in ISO format
+   * @param {string} endDate date(without time) string in ISO format
+   * @returns {Array<string>}
+   */
+  #composeDateRange(startDateISO, endDateISO) {
+    const dateRange = [];
+    const { startDate, endDate } = this.#parseRange(startDateISO, endDateISO)
+    while (startDate <= endDate) {
+      dateRange.push(this.#retrieveISODate(startDate));
+      startDate.setDate(startDate.getDate() + 1)
+    }
+    return dateRange;
+  }
+
+
+  /**
+   * @typedef {object} ParsedRange
+   * @property {number} startDate parsed timestamp
+   * @property {number} endDate parsed timestamp
+   */
+  /**
+   * 
+   * @param {string} startDateISO 
+   * @param {string} endDateISO 
+   * @returns {ParsedRange}
+   */
+  #parseRange(startDateISO, endDateISO) {
+    let startDateTimestamp = Date.parse(startDateISO);
+    let endDateTimestamp = Date.parse(endDateISO);
+
+    if (startDateTimestamp && endDateTimestamp) {
+      if (endDateTimestamp < startDateTimestamp) {
+        startDateTimestamp = Date.now();
+        endDateTimestamp = Date.now();
+      }
+    } else if (startDateTimestamp) {
+      endDateTimestamp = startDateTimestamp;
+    } else if (endDateTimestamp) {
+      startDateTimestamp = endDateTimestamp;
+    } else {
+      startDateTimestamp = Date.now();
+      endDateTimestamp = Date.now();
     }
 
-    #userActivityDataAccess;
-
-    /**
-     * @public
-     * @constructor
-     * @param {UserActivityDataAccess} userActivityDataAccess
-     */
-    constructor (userActivityDataAccess) {
-        this.#userActivityDataAccess = userActivityDataAccess;
+    return {
+      startDate: new Date(startDateTimestamp),
+      endDate: new Date(endDateTimestamp)
     }
+  }
 
-    /**
-     * @description if user has been successfully tracked will return true in another cases returns false
-     * @param {string} id 
-     * @param {string} ip 
-     * @returns {boolean}
-     */
-    track(id, ip) {
-        //FIXME: implement
-    }
+  /**
+   * @description retrieve only date(without time) string in ISO format from Date instance
+   * @param {Date} date 
+   * @returns {string} date(without time) string in ISO format
+   */
+  #retrieveISODate(date) {
+    return date.toISOString().split('T')[0];
+  }
 
-
-    /**
-     * @typedef {Object.<string, number>} UserActivityStats
-     */
-
-    /**
-     * @description return user activity stats
-     * @returns {UserActivityStats}
-     */
-    getStat() {
-        //FIXME: implement
-    }
 }
 
 module.exports = UserActivityService;
---
+++ src/components/user-activity/service.test.js
@@ -0,0 +1,123 @@
+const chai = require('chai');
+const spies = require('chai-spies');
+
+const UserActivityService = require('./service');
+
+
+chai.use(spies);
+const { expect } = chai;
+
+const dataAccessMocked = {
+  addTrackPair: async (id, ip) => { },
+  addTrackId: async (id) => { },
+  addTrackIp: async (ip) => { },
+  getStat: async (date) => { }
+};
+const userActivityService = new UserActivityService(dataAccessMocked);
+
+
+describe('UserActivityService', () => {
+  describe('track', () => {
+    it('if id and ip specified call data access layer method "addTrackPair" with appropriate id and ip', async () => {
+      const addTrackPairSpy = chai.spy.on(dataAccessMocked, 'addTrackPair');
+      const id = 'testId';
+      const ip = 'testIp';
+      await userActivityService.track(id, ip);
+      expect(addTrackPairSpy).to.have.been.called.with(id, ip);
+    });
+
+    it('if only id specified call data access layer method "addTrackId" with appropriate id', async () => {
+      const addTrackIdSpy = chai.spy.on(dataAccessMocked, 'addTrackId');
+      const id = 'testId';
+      await userActivityService.track(id);
+      expect(addTrackIdSpy).to.have.been.called.with(id);
+    });
+
+    it('if only ip specified call data access layer method "addTrackIp" with appropriate ip', async () => {
+      const addTrackIpSpy = chai.spy.on(dataAccessMocked, 'addTrackIp');
+      const ip = 'testIp';
+      await userActivityService.track(undefined, ip);
+      expect(addTrackIpSpy).to.have.been.called.with(ip);
+    });
+
+    it('if no id and ip specified return false', async () => {
+      const isTracked = await userActivityService.track();
+      expect(isTracked).to.be.equal(false);
+    });
+
+  });
+
+  describe('getStat', () => {
+    it(`if startDate and endDate specified in a correct way 
+        call data access layer method "getStat" appropriate times
+        with appropriate dates`, async () => {
+      const getStatSpy = chai.spy.on(dataAccessMocked, 'getStat');
+      const dates = [
+        '2020-08-12',
+        '2020-08-13',
+        '2020-08-14',
+      ];
+      await userActivityService.getStat(dates[0], dates[2]);
+      chai.spy.restore(dataAccessMocked);
+      expect(getStatSpy).to.have.been.first.called.with(dates[0]);
+      expect(getStatSpy).to.have.been.second.called.with(dates[1]);
+      expect(getStatSpy).to.have.been.third.called.with(dates[2]);
+      expect(getStatSpy).to.have.been.called.exactly(3);
+    });
+
+    it('if no startDate date specified or incorrect call data access layer method "getStat" with endDate', async () => {
+      const getStatSpy = chai.spy.on(dataAccessMocked, 'getStat');
+      const dates = [
+        '2020-08-124',
+        '2020-08-13',
+        '2020-08-14',
+      ];
+      await userActivityService.getStat(dates[0], dates[2]);
+      chai.spy.restore(dataAccessMocked);
+      expect(getStatSpy).to.have.been.first.called.with(dates[2]);
+      expect(getStatSpy).to.have.been.called.exactly(1);
+    });
+
+    it('if no endDate specified or incorrect call data access layer method "getStat" with startDate', async () => {
+      const getStatSpy = chai.spy.on(dataAccessMocked, 'getStat');
+      const dates = [
+        '2020-08-12',
+        '2020-08-13',
+        '2020-08-146',
+      ];
+      await userActivityService.getStat(dates[0], dates[2]);
+      chai.spy.restore(dataAccessMocked);
+      expect(getStatSpy).to.have.been.first.called.with(dates[0]);
+      expect(getStatSpy).to.have.been.called.exactly(1);
+    });
+
+    it('if both are not specified or incorrect call data access layer method "getStat" with today\' date', async () => {
+      const getStatSpy = chai.spy.on(dataAccessMocked, 'getStat');
+      const dates = [
+        '2020-08-125',
+        '2020-08-13',
+        '2020-08-146',
+      ];
+      await userActivityService.getStat(dates[0], dates[2]);
+      chai.spy.restore(dataAccessMocked);
+      const currentISODate = (new Date).toISOString().split('T')[0];
+      expect(getStatSpy).to.have.been.first.called.with(currentISODate);
+      expect(getStatSpy).to.have.been.called.exactly(1);
+    });
+
+    it('If date order is incorrect call data access layer method "getStat" with today\' date', async () => {
+      const getStatSpy = chai.spy.on(dataAccessMocked, 'getStat');
+      const dates = [
+        '2020-08-14',
+        '2020-08-13',
+        '2020-08-12',
+      ];
+      await userActivityService.getStat(dates[0], dates[2]);
+      chai.spy.restore(dataAccessMocked);
+      const currentISODate = (new Date).toISOString().split('T')[0];
+      expect(getStatSpy).to.have.been.first.called.with(currentISODate);
+      expect(getStatSpy).to.have.been.called.exactly(1);
+    });
+
+  });
+});
