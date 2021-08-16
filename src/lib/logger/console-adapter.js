585202b4e9440626f672b9145da7d866d40194e4
Your Name <you@example.com>
2021-08-16 12:13:05 +0300

    implement logger

---
+++ src/lib/logger/console-adapter.js
@@ -0,0 +1,108 @@
+//#region : describe Logger interface
+/**
+ * @description
+ * @callback LogFunction
+ * @param {string} msg
+ * @returns {void}
+ */
+
+/**
+ *
+ * @typedef {object} Logger
+ * @property {LogFunction} debug
+ * @property {LogFunction} info
+ * @property {LogFunction} warn
+ * @property {LogFunction} error
+ */
+//#endregion : describe Logger interface
+
+
+//#region : describe Logger config interface
+/**
+ *
+ * @typedef {object} ConsoleAdapterConfig
+ * @property {string} hostName
+ * @property {string} serviceName
+ */
+//#endregion : describe Logger config interface
+
+/**
+ * console implimentation of logger interface
+ */
+class ConsoleAdapter {
+  /**
+   * @public
+   * @static
+   * @param {Console} console
+   * @param {ConsoleAdapterConfig} config
+   */
+  static instantiate(console, config) {
+    return new ConsoleAdapter(console, config);
+  }
+
+  #console;
+  #config;
+
+  /**
+   * @public
+   * @constructor
+   * @param {Console} console
+   * @param {ConsoleAdapterConfig} config
+   */
+  constructor(console, config) {
+    this.#console = console;
+    this.#config = config;
+  }
+
+
+  /**
+   * @description debug level log
+   * @param {string} msg
+   * @returns {void}
+   */
+  debug(msg) {
+    return this.#console.log(this.#composeMessage('debug', msg));
+  }
+
+  /**
+   * @description info level log
+   * @param {string} msg
+   * @returns {void}
+   */
+  info(msg) {
+    return this.#console.log(this.#composeMessage('info', msg));
+  }
+
+  /**
+   * @description warn level log
+   * @param {string} msg
+   * @returns {void}
+   */
+  warn(msg) {
+    return this.#console.log(this.#composeMessage('warn', msg));
+  }
+
+  /**
+   * @description error level log
+   * @param {string} msg
+   * @returns {void}
+   */
+  error(msg) {
+    return this.#console.log(this.#composeMessage('error', msg));
+  }
+
+  /**
+   * 
+   * @param {string} level 
+   * @param {string} msg 
+   * @returns  {string}
+   */
+  #composeMessage(level, msg) {
+    const date = new Date();
+    return `${date.toISOString()}: ${level}: ${this.#config.hostName}: ${this.#config.serviceName}]: ${msg}`
+  }
+
+}
+
+module.exports = ConsoleAdapter;
+
---
+++ src/lib/logger/console-adapter.test.js
@@ -0,0 +1,76 @@
+const chai = require('chai');
+const spies = require('chai-spies');
+const sinon = require('sinon');
+
+const ConsoleAdapter = require('./console-adapter');
+
+
+chai.use(spies);
+const { expect } = chai;
+
+const consoleMocked = {
+  log: (msg) => { },
+
+};
+const config = {
+  hostName: 'testHostName',
+  serviceName: 'testServiceName',
+}
+
+const now = new Date();
+let clock;
+
+const consoleAdapter = new ConsoleAdapter(consoleMocked, config);
+
+
+describe('ConsoleAdapter', () => {
+  
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(now.getTime());
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+
+  describe('debug', () => {
+    it(`call console.log with appropriate msg`, async () => {
+      const logSpy = chai.spy.on(consoleMocked, 'log');
+      const msg = 'testMsg';
+      consoleAdapter.debug(msg);
+      chai.spy.restore(consoleMocked);
+      expect(logSpy).to.have.been.called.with(`${(new Date).toISOString()}: debug: ${config.hostName}: ${config.serviceName}]: ${msg}`);
+    });
+
+  });
+
+  describe('info', () => {
+    it(`call console.log with appropriate msg`, async () => {
+      const logSpy = chai.spy.on(consoleMocked, 'log');
+      const msg = 'testMsg';
+      consoleAdapter.info(msg);
+      chai.spy.restore(consoleMocked);
+      expect(logSpy).to.have.been.called.with(`${(new Date).toISOString()}: info: ${config.hostName}: ${config.serviceName}]: ${msg}`);
+    });
+  });
+
+  describe('warn', () => {
+    it(`call console.log with appropriate msg`, async () => {
+      const logSpy = chai.spy.on(consoleMocked, 'log');
+      const msg = 'testMsg';
+      consoleAdapter.warn(msg);
+      chai.spy.restore(consoleMocked);
+      expect(logSpy).to.have.been.called.with(`${(new Date).toISOString()}: warn: ${config.hostName}: ${config.serviceName}]: ${msg}`);
+    });
+  });
+
+  describe('error', () => {
+    it(`call console.log with appropriate msg`, async () => {
+      const logSpy = chai.spy.on(consoleMocked, 'log');
+      const msg = 'testMsg';
+      consoleAdapter.error(msg);
+      chai.spy.restore(consoleMocked);
+      expect(logSpy).to.have.been.called.with(`${(new Date).toISOString()}: error: ${config.hostName}: ${config.serviceName}]: ${msg}`);
+    });
+  });
+});
