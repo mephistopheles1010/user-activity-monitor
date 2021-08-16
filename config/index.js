fe63c210b0aafee6d9749ffaab668f002a4379f1
Your Name <you@example.com>
2021-08-16 13:42:46 +0300

    implement application config

---
+++ config/index.js
@@ -0,0 +1,25 @@
+const env = process.env;
+
+const config = {};
+
+config.server = {
+  port: env.SERVER_PORT,
+};
+
+config.redis = {
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+};
+
+if (env.REDIS_USER && env.REDIS_PASSWORD) {
+  config.redis.user = env.REDIS_USER,
+    config.redis.password = env.REDIS_PASSWORD
+}
+
+config.logger = {
+  hostName: env.LOGGER_HOST_NAME,
+  serviceName: env.LOGGER_SERVICE_NAME,
+};
+
+module.exports = config;
